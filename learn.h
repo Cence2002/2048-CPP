1d747e6a35b3c32b48283c4347bac83a414b1c2f
Bence Hervay <hervay.bence@gmail.com>
2023-12-31 23:31:33 +0100

    Multithreading test on VM - no stats

--- learn.h
+++ learn.h
@@ -50,6 +50,7 @@ Game_stat training_episode() {
 
 template<u8 N>
 Game_stat testing_episode() {
+    cnt++;
     u64 board = 0;
     s_t score = 0;
     u32 moves = 0;
@@ -132,6 +133,7 @@ vector<Game_stat> run_testing_episodes(u32 games, u8 threads) {
     srand(42);
     cout << "Testing started (" << games << " games)" << endl;
 
+    cnt = 0;
     run_stats = {};
     vector<Game_stat> games_stats;
     auto start = time_now();
@@ -140,7 +142,7 @@ vector<Game_stat> run_testing_episodes(u32 games, u8 threads) {
             games_stats.push_back(testing_episode<N>());
         }
     } else {
-        vector<thread> all_threads;
+        /*vector<thread> all_threads;
         const u32 threads_games = games / threads;
         vector<vector<Game_stat>> threads_stats(threads);
         for (u8 t = 0; t < threads; ++t) {
@@ -155,8 +157,22 @@ vector<Game_stat> run_testing_episodes(u32 games, u8 threads) {
         }
         for (const auto &thread_stats: threads_stats) {
             games_stats.insert(games_stats.end(), thread_stats.begin(), thread_stats.end());
+        }*/
+        //simply run testing in threads without saving stats
+        vector<thread> all_threads;
+        const u32 threads_games = games / threads;
+        for (u8 t = 0; t < threads; ++t) {
+            all_threads.emplace_back([t, threads_games]() {
+                for (u32 i = 0; i < threads_games; ++i) {
+                    testing_episode<N>();
+                }
+            });
+        }
+        for (auto &thread: all_threads) {
+            thread.join();
         }
     }
+    cout << "cnt: " << cnt << endl;
     testing_stats = {};
     for (const auto &game_stats: games_stats) {
         testing_stats.update_board_stats(game_stats.board, game_stats.score, game_stats.moves);
